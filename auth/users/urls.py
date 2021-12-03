081bdf8a79cd5b8e2b0b737776047e1d6b5f2447
Rushi Kadlag <62883177+rushikadlag0911@users.noreply.github.com>
2021-12-03 18:00:39 +0530

    pyjwt-project

---
+++ auth/users/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import LoginView, RegisterView
+
+urlpatterns = [
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
+]
